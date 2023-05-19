6c8ad2d3847a7c7828bd37473620bef8f9c74ca0
dhythm <yuta.okada.20@gmail.com>
2023-05-20 01:34:19 +0900

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,14 @@
-import { Button, Form, Input, Select, Space } from "antd";
+import {
+  Button,
+  Card,
+  Col,
+  Form,
+  Input,
+  Row,
+  Select,
+  Space,
+  Typography,
+} from "antd";
 import {
   MinusCircleOutlined,
   PlusCircleOutlined,
@@ -8,103 +18,156 @@ import {
 function App() {
   const [form] = Form.useForm();
   const values = Form.useWatch([], form);
-  const options = values?.names?.filter((v) => v) ?? [];
+  const options = values?.blocks?.filter((v) => v.block) ?? [];
 
   return (
     <Form
       form={form}
       name="dynamic_form_item"
-      initialValues={{ names: [""], flows: [""] }}
+      initialValues={{ blocks: [{ items: [""] }], flows: [""] }}
       onFinish={(values) => console.log(values)}
       style={{ maxWidth: 600 }}
     >
       <Space direction="vertical">
-        <Form.List name="names">
+        <Form.List name="blocks">
           {(fields, { add, remove }, { errors }) => {
             return (
               <Space direction="vertical">
                 {fields.map((field, index) => (
-                  <Form.Item noStyle>
-                    <Space key={field.key} align="baseline">
-                      <Form.Item
-                        {...field}
-                        name={[field.name, "name"]}
-                        rules={[{ required: true, whitespace: true }]}
-                        noStyle
-                      >
-                        <Input placeholder="" style={{ width: "100%" }} />
+                  <Card title={`Node ${index + 1}`}>
+                    <Form.Item key={`blocks-${fields.key}`} noStyle>
+                      <Space align="baseline">
+                        <Form.Item
+                          {...field}
+                          name={[field.name, "block"]}
+                          rules={[{ required: true, whitespace: true }]}
+                          noStyle
+                        >
+                          <Input style={{ width: "100%" }} />
+                        </Form.Item>
+                        {fields.length > 1 && fields.length - 1 !== index ? (
+                          <MinusCircleOutlined
+                            onClick={() => remove(field.name)}
+                          />
+                        ) : (
+                          <PlusCircleOutlined
+                            onClick={() => add({ items: [""] })}
+                          />
+                        )}
+                      </Space>
+                      <Form.Item style={{ marginTop: 8 }}>
+                        <Form.List name={[field.name, "items"]}>
+                          {(_fields, { add: _add, remove: _remove }) => {
+                            return (
+                              <Space direction="vertical" size="small">
+                                {_fields.map((_field, idx) => (
+                                  <Space
+                                    key={`items-${_field.key}`}
+                                    align="baseline"
+                                    style={{ marginLeft: 24 }}
+                                  >
+                                    <Form.Item
+                                      {..._field}
+                                      name={[_field.name, "item"]}
+                                      fieldId={[_field.fieldKey, "item"]}
+                                      key={idx}
+                                      style={{ margin: 0 }}
+                                    >
+                                      <Input style={{ width: "100%" }} />
+                                    </Form.Item>
+                                    {_fields.length > 1 &&
+                                    _fields.length - 1 !== idx ? (
+                                      <MinusCircleOutlined
+                                        onClick={() => _remove(_field.name)}
+                                      />
+                                    ) : (
+                                      <PlusCircleOutlined
+                                        onClick={() => _add()}
+                                      />
+                                    )}
+                                  </Space>
+                                ))}
+                              </Space>
+                            );
+                          }}
+                        </Form.List>
                       </Form.Item>
-                      {fields.length > 1 && fields.length - 1 !== index ? (
-                        <MinusCircleOutlined
-                          onClick={() => remove(field.name)}
-                        />
-                      ) : (
-                        <PlusCircleOutlined onClick={() => add()} />
-                      )}
-                    </Space>
-                  </Form.Item>
+                    </Form.Item>
+                  </Card>
                 ))}
               </Space>
             );
           }}
         </Form.List>
 
-        <Form.List name="flows">
-          {(fields, { add, remove }, { errors }) => {
-            return (
-              <Space direction="vertical">
-                {fields.map((field, index) => (
-                  <Form.Item noStyle>
-                    <Space key={field.key} align="baseline">
-                      <Form.Item {...field} name={[field.name, "from"]}>
-                        <Select
-                          disabled={options.length === 0}
-                          style={{ width: 130 }}
+        <Card title="Relationship">
+          <Form.List name="flows">
+            {(fields, { add, remove }, { errors }) => {
+              return (
+                <Space direction="vertical">
+                  {fields.map((field, index) => (
+                    <Form.Item noStyle>
+                      <Space key={`flows-${field.key}`} align="baseline">
+                        <Form.Item
+                          {...field}
+                          name={[field.name, "from"]}
+                          style={{ margin: 0 }}
                         >
-                          {options.map((option, idx) => {
-                            return (
-                              <Select.Option key={idx} value={option.name}>
-                                {option.name}
-                              </Select.Option>
-                            );
-                          })}
-                        </Select>
-                      </Form.Item>
-                      <ArrowRightOutlined />
-                      <Form.Item {...field} name={[field.name, "to"]}>
-                        <Select
-                          disabled={options.length === 0}
-                          style={{ width: 130 }}
+                          <Select
+                            disabled={options.length === 0}
+                            style={{ width: 130 }}
+                          >
+                            {options.map((option, idx) => {
+                              return (
+                                <Select.Option key={idx} value={option.block}>
+                                  {option.block}
+                                </Select.Option>
+                              );
+                            })}
+                          </Select>
+                        </Form.Item>
+                        <ArrowRightOutlined />
+                        <Form.Item
+                          {...field}
+                          name={[field.name, "to"]}
+                          style={{ margin: 0 }}
                         >
-                          {options.map((option, idx) => {
-                            return (
-                              <Select.Option key={idx} value={option.name}>
-                                {option.name}
-                              </Select.Option>
-                            );
-                          })}
-                        </Select>
-                      </Form.Item>
-                      {fields.length > 1 && fields.length - 1 !== index ? (
-                        <MinusCircleOutlined
-                          onClick={() => remove(field.name)}
-                        />
-                      ) : (
-                        <PlusCircleOutlined onClick={() => add()} />
-                      )}
-                    </Space>
-                  </Form.Item>
-                ))}
-              </Space>
-            );
-          }}
-        </Form.List>
+                          <Select
+                            disabled={options.length === 0}
+                            style={{ width: 130 }}
+                          >
+                            {options.map((option, idx) => {
+                              return (
+                                <Select.Option key={idx} value={option.block}>
+                                  {option.block}
+                                </Select.Option>
+                              );
+                            })}
+                          </Select>
+                        </Form.Item>
+                        {fields.length > 1 && fields.length - 1 !== index ? (
+                          <MinusCircleOutlined
+                            onClick={() => remove(field.name)}
+                          />
+                        ) : (
+                          <PlusCircleOutlined onClick={() => add()} />
+                        )}
+                      </Space>
+                    </Form.Item>
+                  ))}
+                </Space>
+              );
+            }}
+          </Form.List>
+        </Card>
 
         <Form.Item>
           <Button type="primary" htmlType="submit">
             Submit
           </Button>
         </Form.Item>
+
+        <Typography>{JSON.stringify(values)}</Typography>
       </Space>
     </Form>
   );
